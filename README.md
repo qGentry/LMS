7b4e985bc3d24513ce84bac3ea56cc67c3127530
Fisin Filipp Gennadevich <f.fisin@tinkoff.ru>
2021-01-16 21:46:17 +0300

    Some style fixes + added travis ci

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # LMS
+
+### This is learning managment system written on Python using sqlite3 as database and flask as engine
+### Installation
+pip install -e .
+### How to run
+python src/elms/bin/start_server.py 
--- setup.py
+++ setup.py
@@ -2,22 +2,16 @@ from setuptools import setup, find_packages
 
 
 def main():
-    console_scripts = [
-        "start_service=elms.bin.start_server:main"
-    ]
     with open('requirements.txt') as f:
         required = f.read().splitlines()
     setup(
-        name='eLMS',
+        name='elms',
         version='1.0',
         author='Philipp Fisin',
         packages=find_packages("src"),
         package_dir={'': 'src'},
         description='Backed for Learning Management System with HTTP API',
         install_requires=required,
-        entry_points={
-            'console_scripts': console_scripts,
-        }
     )
 
 
